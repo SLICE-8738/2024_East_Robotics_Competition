0845fbcab80d3b29eb9c19a3372d984f43d89354
IcePenguin1 <130691092+IcePenguin1@users.noreply.github.com>
2024-11-07 15:20:46 -0500

    DRIVETRAIN CODE

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Drivetrain;
+
+public class Drive extends Command {
+
+  private Drivetrain m_drivetrain;
+  private CommandXboxController m_driverController;
+
+  /** Creates a new drive. */
+  public Drive(Drivetrain dT, CommandXboxController cont) {
+    m_drivetrain = dT;
+    m_driverController = cont;
+    
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drivetrain.drive(m_driverController.getLeftY(), m_driverController.getRightX());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drivetrain.drive(0, 0);
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,56 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Drivetrain extends SubsystemBase {
+
+  private CANSparkMax frontLeft, frontRight, backLeft, backRight;
+  private RelativeEncoder frontLeftEncoder, frontRightEncoder, backLeftEncoder, backRightEncoder;
+  private DifferentialDrive driveTrain;
+
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+
+    frontLeft = new CANSparkMax(15, MotorType.kBrushless);
+    frontRight = new CANSparkMax(17, MotorType.kBrushless);
+    backLeft = new CANSparkMax(3, MotorType.kBrushless);
+    backRight = new CANSparkMax(18, MotorType.kBrushless);
+
+    frontLeft.setSmartCurrentLimit(35);
+    frontRight.setSmartCurrentLimit(35);
+    backLeft.setSmartCurrentLimit(35);
+    backRight.setSmartCurrentLimit(35);
+
+    frontLeftEncoder = frontLeft.getEncoder();
+    frontRightEncoder = frontRight.getEncoder();
+    backLeftEncoder = backLeft.getEncoder();
+    backRightEncoder = backRight.getEncoder();
+
+    backLeft.follow(frontLeft);
+    backRight.follow(frontRight);
+
+    driveTrain = new DifferentialDrive(frontLeft, frontRight);
+  }
+
+  public void drive(double forwardSpeed, double rotationSpeed){
+    driveTrain.arcadeDrive(forwardSpeed, rotationSpeed);
+  }
+    
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+}
