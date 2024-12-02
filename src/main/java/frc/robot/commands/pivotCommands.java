aa123fc815ed41e7def51635cb2d92b91a2dfec2
ryanstutav <dakotapercy35@gmail.com>
2024-12-02 15:03:26 -0500

    Added pivotSubsystem and pivotCommands

---
+++ src/main/java/frc/robot/commands/pivotCommands.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.pivotSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class pivotCommands extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  public final pivotSubsystem pivotCommand;
+  Joystick joystick = new Joystick(0);
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public pivotCommands(pivotSubsystem subsystem) {
+    pivotCommand = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println("Trying to start pivot arm...");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      double bothwaysSpeed = joystick.getX();
+      pivotCommand.pivot(bothwaysSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/pivotSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class pivotSubsystem extends SubsystemBase{
+    // temporary device ID and motortype
+    public final PWMSparkMax pivotMotor = new PWMSparkMax(6);
+
+    public pivotSubsystem() {
+    }
+
+    public void pivot(double speed) {
+        pivotMotor.set(speed);
+    }
+    @Override
+    public void periodic() {
+
+    }
+    
+}
