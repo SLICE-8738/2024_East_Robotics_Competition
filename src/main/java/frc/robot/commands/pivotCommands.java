9f5c9afcd41ca625428b15ab0b63f3328a881ef9
Justme <87026908+Code2-life@users.noreply.github.com>
2024-12-02 15:11:02 -0500

    pivotCommands.java

--- src/main/java/frc/robot/commands/pivotCommands.java
+++ src/main/java/frc/robot/commands/pivotCommands.java
@@ -5,8 +5,8 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.pivotSubsystem;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 
 /** An example command that uses an example subsystem. */
 public class pivotCommands extends Command {
